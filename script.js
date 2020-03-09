5bf8b0a5cfc9cb16102b9019443c63b5a0e46bff
dj_mnlpt <darwin_xpanse@gmail.com>
2020-03-09 17:12:42 +0800

    march 9 commit

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-
 let acc = document.getElementsByClassName("accordion");
 
     for (let i = 0; i < acc.length; i++) {
     
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
-           
-            let panel1 = this.nextElementSibling;
-            if (panel1.style.display === "none") {
-                panel1.style.display = "block";     
-            } 
-            else {
-                panel1.style.display = "none";
+            let arrow = this.querySelector(".arrow");
+            let panel = this.nextElementSibling;
+        
+            if (panel.style.display === "none") {
+                panel.style.display = "block";
+                arrow.classList.remove("up");      
+            } else {
+                panel.style.display = "none";
+                arrow.classList.add("up");
             }
         });
     }
