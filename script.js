6e22f2efebd6ff359b85c57a1933fd48fa41bb1c
dj_mnlpt <darwin_xpanse@gmail.com>
2020-03-11 17:55:16 +0800

    march 11 commit

--- script.js
+++ script.js
@@ -7,13 +7,62 @@ let acc = document.getElementsByClassName("accordion");
             let arrow = this.querySelector(".arrow");
             let panel = this.nextElementSibling;
         
-            if (panel.style.display === "none") {
-                panel.style.display = "block";
-                arrow.classList.remove("up");      
+            if (panel.style.maxHeight) {
+                // panel.style.display = "block";
+                arrow.classList.remove("up");
+                panel.style.maxHeight = null;
+                panel.style.marginBottom = 0 + "px";  
+                
             } else {
-                panel.style.display = "none";
+                // panel.style.display = "none";
                 arrow.classList.add("up");
+                panel.style.maxHeight = panel.scrollHeight + "px";  
+                panel.style.marginBottom = 20 + "px";          
             }
         });
     }
+    $(".accordion").trigger('click');
 
+
+let slider = document.getElementById("priceRangeMin");
+let output = document.getElementById("priceInputMin");
+    output.value = " " + slider.value + ".00";
+
+    slider.oninput = function () {
+    output.value = " " + this.value + ".00";
+        }
+
+    output.oninput = function (){
+    slider.value = this.value; 
+    }
+
+let slider1 = document.getElementById("priceRangeMax");
+let output1 = document.getElementById("priceInputMax");
+    output1.value = " " + slider1.value + ".00";
+
+    slider1.oninput = function () {
+        output1.value = " " + this.value + ".00";
+        }
+
+    output1.oninput = function (){
+        slider1.value = this.value;
+        }
+
+let mobileBtn = document.getElementsByClassName("mobile-button");
+
+    for (let j = 0; j < mobileBtn.length; j++) {
+
+        mobileBtn[j].addEventListener("click", function() {
+            this.classList.toggle("active");
+           
+            let filterPanel = this.nextElementSibling;
+            console.log(filterPanel);
+            if (filterPanel.style.maxHeight) {      
+                
+                filterPanel.style.maxHeight = null; 
+                
+            } else  {
+                filterPanel.style.maxHeight = filterPanel.scrollHeight + "px";
+            }
+        });
+    }
